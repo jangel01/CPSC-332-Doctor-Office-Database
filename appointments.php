c544f1be2281ec5b70c28c6e9780ad28e171974c
Tokkisan <74226172+Tokkisan@users.noreply.github.com>
2022-12-05 02:10:41 -0800

    Update appointments.php

--- appointments.php
+++ appointments.php
@@ -8,9 +8,24 @@
 <?php
             $server = 'localhost';
             $username = 'root';
-            $password = '';
+            $password = 'Lee20704!';
             $database = 'docOffice';
             $connection = new mysqli($server, $username, $password, $database, 3306) or die("not connected");
+
+?>
+<form action = "appointments.php" method = "get">
+Appointment number: <input type = "text" name = "num">
+<input type = "submit">
+</form>
+
+<?php
+
+        $result = NULL;
+        $result2 = NULL;
+
+        if(isset($_GET['num'])) {
+
+            $num = $_GET['num'];
 
             $sql = "select Appointment.Appointment_Number, Appointment.Date, Appointment.Time, Appointment.Doctor_Name, Doctor.Phone_Number, Doctor.Specialty, Patient.First_Name, Patient.Last_Name, Appointment.Room_Number
             from Appointment, Doctor, Patient
@@ -32,31 +47,77 @@
             <th> Room Number </th>
             </tr>";
 
-            while($row = mysqli_fetch_array($result))
-            {
-
-                echo "<tr>";
-                echo "<td>".$row['Appointment_Number']."</td>";
-                echo "<td>".$row['Date']."</td>";
-                echo "<td>".$row['Time']."</td>";
-                echo "<td>".$row['Doctor_Name']."</td>";
-                echo "<td>".$row['Phone_Number']."</td>";
-                echo "<td>".$row['Specialty']."</td>";
-                echo "<td>".$row['First_Name']."</td>";
-                echo "<td>".$row['Last_Name']."</td>";
-                echo "<td>".$row['Room_Number']."</td>";
-
+                while($row = mysqli_fetch_array($result))
+                {
+                    if($row['Appointment_Number'] == $num) {
+                    echo "<tr>";
+                    echo "<td>".$row['Appointment_Number']."</td>";
+                    echo "<td>".$row['Date']."</td>";
+                    echo "<td>".$row['Time']."</td>";
+                    echo "<td>".$row['Doctor_Name']."</td>";
+                    echo "<td>".$row['Phone_Number']."</td>";
+                    echo "<td>".$row['Specialty']."</td>";
+                    echo "<td>".$row['First_Name']."</td>";
+                    echo "<td>".$row['Last_Name']."</td>";
+                    echo "<td>".$row['Room_Number']."</td>";
+                }
             }
-            echo "</table>";
-            echo "</pre>";
+        echo "</pre>";
+        echo "</table>";
+            
+        } else {
 
+            $sql2 = "select Appointment.Appointment_Number, Appointment.Date, Appointment.Time, Appointment.Doctor_Name, Doctor.Phone_Number, Doctor.Specialty, Patient.First_Name, Patient.Last_Name, Appointment.Room_Number
+            from Appointment, Doctor, Patient
+            where Patient.SSN = Appointment.Patient_SSN and Appointment.Doctor_Name = Doctor.First_Name
+            order by Appointment.Appointment_Number asc";
+
+            $result2 = mysqli_query($connection, $sql2);
+            echo "<pre>";
+            echo "<table border = 1>
+            <tr>
+            <th> Appointment #</th>
+            <th> Appointment Date</th>
+            <th> Appointment Time</th>
+            <th> Doctor Name</th>
+            <th> Doctor Phone Number</th>
+            <th> Specialty </th>
+            <th> Patient First Name</th>
+            <th> Patient Last Name</th>
+            <th> Room Number </th>
+            </tr>";
+
+            while($row = mysqli_fetch_array($result2))
+                {
+                    echo "<tr>";
+                    echo "<td>".$row['Appointment_Number']."</td>";
+                    echo "<td>".$row['Date']."</td>";
+                    echo "<td>".$row['Time']."</td>";
+                    echo "<td>".$row['Doctor_Name']."</td>";
+                    echo "<td>".$row['Phone_Number']."</td>";
+                    echo "<td>".$row['Specialty']."</td>";
+                    echo "<td>".$row['First_Name']."</td>";
+                    echo "<td>".$row['Last_Name']."</td>";
+                    echo "<td>".$row['Room_Number']."</td>";
+                }
+                echo "</pre>";
+                echo "</table>";
+            }
+
+            if($result != NULL) {
+            mysqli_free_result($result);
+            }
+            if($result2 != NULL) {
+            mysqli_free_result($result2);
+            }
+            mysqli_close($connection);
 
 
 
 ?>
 
 
-
+        
     </main>
     <?php include('footer.php'); ?>
 </html>
