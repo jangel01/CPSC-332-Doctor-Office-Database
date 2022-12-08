6392ded8051c3fa7c55d6228ef90e139212b68b7
jangel01 <jasonangel@csu.fullerton.edu>
2022-12-07 21:37:52 -0800

    add style to prescriptions.php

--- prescriptions.php
+++ prescriptions.php
@@ -4,11 +4,16 @@
 
     <main style = "min-height: calc(100vh - 128px - 56px);">
 
+    <div class="flex items-center justify-center mb-10 scale-125">
+            <div class = "border-2 border-black bg-slate-900 basis-3/12 text-white rounded-lg mt-10 px-10 py-3">
+                <p>Below is a list of information on prescriptions prescribed to patients by doctors.</p> 
+            </div>
+        </div> 
 
 <?php
             $server = 'localhost';
             $username = 'root';
-            $password = 'Lee20704!';
+            $password = '';
             $database = 'docOffice';
             $connection = new mysqli($server, $username, $password, $database, 3306) or die("not connected");
 
@@ -19,33 +24,66 @@
 
             $result = mysqli_query($connection, $sql);
 
-            echo "<pre>";
-            echo "<table border = 1>
-            <tr>
-            <th> First Name </th>
-            <th> Last Name </th>
-            <th> Drug Name </th>
-            <th> Dosage </th>
-            <th> Date Prescribed </th>
-            <th> Dr.Fname </th>
-            <th> Dr.Lname </th>
-            <th> Dr.PhoneNum </th>
-            </tr>";
+        print "<div class = 'flex items-center justify-center'>";
+            print "<div class='flex flex-ol'>";
+                print "<div class='overflow-x-auto sm:-mx-6 lg:-mx-8'>";
+                    print "<div class='py-4 inline-block min-w-full sm:px-6 lg:px-8'>";
+                        print "<div class='overflow-hidden'>";
+                            print "<table class='min-w-full text-center'>";
+                                print "<thead class='border-b bg-gray-800'>";
+                                    print "<tr>";
+                                        print "<th scope='col' class='text-sm font-medium text-white px-6 py-4'>";
+                                            print "First Name";
+                                        print "</th>";
+                                        print "<th scope='col' class='text-sm font-medium text-white px-6 py-4'>";
+                                            print "Last Name";
+                                        print "</th>";
+                                        print "<th scope='col' class='text-sm font-medium text-white px-6 py-4'>";
+                                            print "Drug Name";
+                                        print "</th>";
+                                        print "<th scope='col' class='text-sm font-medium text-white px-6 py-4'>";
+                                            print "Dosage";
+                                        print "</th>";
+                                        print "<th scope='col' class='text-sm font-medium text-white px-6 py-4'>";
+                                            print "Date Prescribed";
+                                        print "</th>";
+                                        print "<th scope='col' class='text-sm font-medium text-white px-6 py-4'>";
+                                            print "Dr. First Name";
+                                        print "</th>";
+                                        print "<th scope='col' class='text-sm font-medium text-white px-6 py-4'>";
+                                            print "Dr. Last Name";
+                                        print "</th>";
+                                        print "<th scope='col' class='text-sm font-medium text-white px-6 py-4'>";
+                                            print "Dr. Phone Number";
+                                        print "</th>";
+                                    print "</tr>";
+                                print "</thead class='border-b'>";
 
-            while($row = mysqli_fetch_array($result))
-            {
-                echo "<tr>";
-                echo "<td>".$row['First_Name']."</td>";
-                echo "<td>".$row['Last_Name']."</td>";
-                echo "<td>".$row['Drug_Name']."</td>";
-                echo "<td>".$row['Dosage']."</td>";
-                echo "<td>".$row['Date_Prescribed']."</td>";
-                echo "<td>".$row['Dr_Fname']."</td>";
-                echo "<td>".$row['Dr_Lname']."</td>";
-                echo "<td>".$row['Dr_PhoneNum']."</td>";
-            }
-            echo "</table>";
-            echo "</pre>";
+                                print "<tbody>";
+                                    while($row = mysqli_fetch_array($result))
+                                    {
+                                        print "<tr class='bg-white border-b'>";
+                                            print "<td class='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>$row[First_Name]</td>";
+                                            print "<td class='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>$row[Last_Name]</td>";
+                                            print "<td class='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>$row[Drug_Name]</td>";
+                                            print "<td class='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>$row[Dosage]</td>";
+                                            print "<td class='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>$row[Date_Prescribed]</td>";
+                                            print "<td class='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>$row[Dr_Fname]</td>";
+                                            print "<td class='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>$row[Dr_Lname]</td>";
+                                            print "<td class='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>$row[Dr_PhoneNum]</td>";
+                                        print "</tr class='bg-white border-b'>";
+                                    }
+                                print "</tbody>";
+                            print "</tbody>";
+                            print "</table>";
+                        print "</div>";
+                    print "</div>";
+                print "</div>";
+            print "</div>";
+        print "</div>";
+            
+
+            
 
 
 ?>
