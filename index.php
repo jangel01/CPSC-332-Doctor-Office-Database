7fa1c5872bddb75ac554ea8d7d8dc23b3e273a61
jangel01 <jasonangel@csu.fullerton.edu>
2022-12-08 00:03:54 -0800

    text stuff

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
           <div class="flex justify-center items-center h-full">
             <div class="text-center text-white px-6 md:px-12 w-7/12">
               <h1 class="text-5xl font-bold mt-0 mb-6">Welcome!</h1>
-              <h3 class="text-3xl font-bold mb-8">We are happy to assist you for your medical needs. Create an account to look up your appointments, access medical records, and much more! </h3>
+              <h3 class="text-3xl font-semibold mb-8">We are happy to assist you for your medical needs. Create an account to look up your appointments, access medical records, and much more! </h3>
                
               <button type="button"
                 class="inline-block px-6 py-2.5 border-2 border-white text-white font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
@@ -27,6 +27,8 @@
       </div>
       <!-- Background image -->
     
+      <h1 class="font-semibold leading-tight text-5xl mt-0 mb-2 text-grey-600 bold text-left mt-10 ml-5">How can we help you today?</h1>
+
         <div class="flex items-stretchjustify-center mb-10 space-x-20 mt-10 text-center">
             <div class="block p-6 rounded-lg shadow-lg bg-white max-w-sm ">
                 <h5 class="text-gray-900 text-xl leading-tight font-medium mb-5">Find Doctors and Locations</h5>
